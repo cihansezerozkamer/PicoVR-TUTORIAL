e37fd952591ef11a05aeb979ad2f2c3e439b057d
cihan <sezersezo37@gmail.com>
2024-02-08 00:00:07 +0300

    Fire Bullet and Interraction cubes
    - Bullet fire script
    - Interation differences (kinematic-intastant-velocity)
    - Teleportation bug fixing.

---
+++ Assets/FireBulletOnActivate.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FireBulletOnActivate : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public float fireSpeed = 20;
+
+    void Start()
+    {
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(FireBullet);
+    }
+
+    private void FireBullet(ActivateEventArgs arg)
+    {
+        GameObject spawnedBullet = Instantiate(bullet);
+        spawnedBullet.transform.position = spawnPoint.position;
+        spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+        Destroy(spawnedBullet,5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
